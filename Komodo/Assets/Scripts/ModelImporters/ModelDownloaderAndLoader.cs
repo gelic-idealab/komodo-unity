def2ea3d676e557b12be91927149211f48af3254
bcdang2 <bcdang2@illinois.edu>
2021-02-22 14:41:18 -0600

    fix indentation

--- Komodo/Assets/Scripts/ModelImporters/ModelDownloaderAndLoader.cs
+++ Komodo/Assets/Scripts/ModelImporters/ModelDownloaderAndLoader.cs
@@ -9,80 +9,80 @@ using UnityEngine.UI;
 
 namespace Komodo.AssetImport
 {
-public class ModelDownloaderAndLoader : MonoBehaviour
-{
-    //Reference for the latest loaded GameObject
-    protected GameObject loadedGameObject;
-    public string urlBase = "https://vrcat-assets.s3.us-east-2.amazonaws.com/";
-
-    public bool logging = false;
-
-    /**
-    * Returns an array: first value is the GUID; second value is the file name and extension.
-    */
-    private string[] getModelParameters(string url) {
-        string urlSuffix = url.Substring(urlBase.Length);
-        string[] modelParams = urlSuffix.Split('/');
-        return modelParams;
-    }
-    
-    /** 
-     * Creates a directory to store the model in and then passes model data onto a download coroutine.
-     */
-    public IEnumerator GetFileFromURL(ModelDataTemplate.ModelImportData modelData, Text progressDisplay, int index, System.Action<GameObject> callback)
+    public class ModelDownloaderAndLoader : MonoBehaviour
     {
-        //Gets guid and filename and extension       
-        string[] modelParams = getModelParameters(modelData.url); 
-        var guid = modelParams[0];
-        var fileNameAndExtension = modelParams[1];
+        //Reference for the latest loaded GameObject
+        protected GameObject loadedGameObject;
+        public string urlBase = "https://vrcat-assets.s3.us-east-2.amazonaws.com/";
 
-        //Create a unique directory based on the guid
-        var localFilePath = $"{Application.persistentDataPath}/{guid}";
+        public bool logging = false;
 
-        Directory.CreateDirectory(localFilePath);
-
-        var localPathAndFilename = $"{localFilePath}/{fileNameAndExtension}";
-
-        if (!File.Exists(localPathAndFilename)) {
-            yield return StartCoroutine(DownloadFile(modelData, progressDisplay, index, localPathAndFilename, callback));
-            yield break;
+        /**
+        * Returns an array: first value is the GUID; second value is the file name and extension.
+        */
+        private string[] getModelParameters(string url) {
+            string urlSuffix = url.Substring(urlBase.Length);
+            string[] modelParams = urlSuffix.Split('/');
+            return modelParams;
         }
-
-        Debug.Log($"{modelData.name} cached. Loading immediately.");
         
-        progressDisplay.text = $"{modelData.name} cached. Loading immediately.";
-
-        LoadLocalFile(localPathAndFilename, callback);
-    }
-
-
-    /** 
-    * Downloads a file to a local path, then loads the file
-    */
-    private IEnumerator DownloadFile(ModelDataTemplate.ModelImportData modelData, Text progressDisplay, int index, string localPathAndFilename, System.Action<GameObject> callback = null)
-    {
-        UnityWebRequest fileDownloader = UnityWebRequest.Get(modelData.url);
-
-        //get size of model first to allocate what is needed
-        long modelSize = 0;
-        yield return StartCoroutine(GetFileSize(modelData.url, (size) =>
-            {
-                modelSize = size;
-            })
-        );
-
-        //set our model download settings
-        fileDownloader.method = UnityWebRequest.kHttpVerbGET;
-        var dh = new DownloadHandlerFile(localPathAndFilename);
-        dh.removeFileOnAbort = true;
-        fileDownloader.downloadHandler = dh;
-        fileDownloader.SendWebRequest();
-
-        while (!fileDownloader.isDone)
+        /** 
+        * Creates a directory to store the model in and then passes model data onto a download coroutine.
+        */
+        public IEnumerator GetFileFromURL(ModelDataTemplate.ModelImportData modelData, Text progressDisplay, int index, System.Action<GameObject> callback)
         {
-            progressDisplay.text = $"Downloading {modelData.name}: {fileDownloader.downloadProgress.ToString("P")}";
-            yield return null;
+            //Gets guid and filename and extension       
+            string[] modelParams = getModelParameters(modelData.url); 
+            var guid = modelParams[0];
+            var fileNameAndExtension = modelParams[1];
+
+            //Create a unique directory based on the guid
+            var localFilePath = $"{Application.persistentDataPath}/{guid}";
+
+            Directory.CreateDirectory(localFilePath);
+
+            var localPathAndFilename = $"{localFilePath}/{fileNameAndExtension}";
+
+            if (!File.Exists(localPathAndFilename)) {
+                yield return StartCoroutine(DownloadFile(modelData, progressDisplay, index, localPathAndFilename, callback));
+                yield break;
+            }
+
+            Debug.Log($"{modelData.name} cached. Loading immediately.");
+            
+            progressDisplay.text = $"{modelData.name} cached. Loading immediately.";
+
+            LoadLocalFile(localPathAndFilename, callback);
         }
+
+
+        /** 
+        * Downloads a file to a local path, then loads the file
+        */
+        private IEnumerator DownloadFile(ModelDataTemplate.ModelImportData modelData, Text progressDisplay, int index, string localPathAndFilename, System.Action<GameObject> callback = null)
+        {
+            UnityWebRequest fileDownloader = UnityWebRequest.Get(modelData.url);
+
+            //get size of model first to allocate what is needed
+            long modelSize = 0;
+            yield return StartCoroutine(GetFileSize(modelData.url, (size) =>
+                {
+                    modelSize = size;
+                })
+            );
+
+            //set our model download settings
+            fileDownloader.method = UnityWebRequest.kHttpVerbGET;
+            var dh = new DownloadHandlerFile(localPathAndFilename);
+            dh.removeFileOnAbort = true;
+            fileDownloader.downloadHandler = dh;
+            fileDownloader.SendWebRequest();
+
+            while (!fileDownloader.isDone)
+            {
+                progressDisplay.text = $"Downloading {modelData.name}: {fileDownloader.downloadProgress.ToString("P")}";
+                yield return null;
+            }
 
 
         //Debug.Log($"Successfully downloaded model {modelData.name}, size {fileDownloader.downloadedBytes} bytes.");
