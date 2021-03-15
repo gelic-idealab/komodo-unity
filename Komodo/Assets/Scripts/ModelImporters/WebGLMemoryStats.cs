15786557b89c1d8ecda8379de525f57e7bdd2af9
bcdang2 <bcdang2@illinois.edu>
2021-03-15 18:43:15 -0500

    created class for runtime memory measurement

--- Komodo/Assets/Scripts/ModelImporters/WebGLMemoryStats.cs
+++ Komodo/Assets/Scripts/ModelImporters/WebGLMemoryStats.cs
@@ -4,13 +4,19 @@ using System.Collections;
 using System.Runtime.InteropServices;
 using UnityEngine.Profiling;
 
-namespace Komodo.AssetImport 
+namespace Komodo.AssetImport
 {
-    public static class WebGLMemoryStats 
+    public static class WebGLMemoryStats
     {
         public static bool doLogStats = false;
 
         public static float numSecondsToWait = 5f;
+
+        public static uint managedHeapUsed;
+
+        public static uint managedHeapTotal;
+
+        public static uint highwaterUsed;
 
         private static uint _memoryLimit;
 
@@ -18,7 +24,7 @@ namespace Komodo.AssetImport
 
         private static uint mobileVsDesktopThreshold = 1024 * 1024 * 1024;
 
-        private static uint mobileMemoryLimit = 128 * 1024 * 1024;
+        private static uint mobileMemoryLimit = 256 * 1024 * 1024;
 
         private static uint desktopMemoryLimit = 0;
 
@@ -43,7 +49,8 @@ namespace Komodo.AssetImport
         [DllImport("__Internal")]
         private static extern uint GetJSHeapMaxSize(bool doLogStats);
 
-        public static void LogStats () {
+        public static void LogStats()
+        {
 #if UNITY_WEBGL && !UNITY_EDITOR
             LogMemoryStatsInMegabytes();
 # else 
@@ -51,67 +58,107 @@ namespace Komodo.AssetImport
 #endif
         }
 
-        public static uint GetHeapMaxSize (bool doLogThis) {
-#if UNITY_WEBGL && !UNITY_EDITOR
-            return GetJSHeapMaxSize(doLogThis);
-# else 
-            Debug.LogWarning("Simulating GetHeapMaxSize(): returning maximum uint Value()");
-            return uint.MaxValue;
-#endif
+        public static void LogMoreStats(string message)
+        {
+            Debug.Log(GetMoreStats(message));
         }
 
-        public static uint GetTotalUsedMemorySize () {
-#if UNITY_WEBGL && !UNITY_EDITOR
-            return GetUsedMemorySize();
-# else 
-            Debug.LogWarning("Simulating GetTotalUsedMemorySize(): returning 0");
-            return 0;
-#endif
+        public static string GetMoreStats(string message)
+        {
+            return ($"{message}: Managed heap: {ToRoundedMB(Profiler.GetMonoUsedSizeLong(), 2)} / {ToRoundedMB(Profiler.GetMonoHeapSizeLong(), 2)}MB.    "
+                + $"Highwater used: {ToRoundedMB(GetTotalUsedMemorySize(), 2)}MB.\n");
         }
-        
-        public static void InitMemoryLimit(uint size) {
+
+        /*
+        * Naive implementation: check the Max JS Heap Size. If it exists, we are on Chrome, and if its value is less than an empirical amount, we should mark ourselves as mobile. 
+        */
+        public static void ChooseMemoryLimitForDevice(bool forceMobileMemoryLimit)
+        {
+            uint heapMaxSize = WebGLMemoryStats.GetHeapMaxSize(true);
+
+            Debug.Log("iFrame JS Heap Size Limit: " + ToRoundedMB(heapMaxSize, 2) + "MB");
+
+            if (heapMaxSize == 0 && !forceMobileMemoryLimit)
+            {
+                _memoryLimit = 0;
+                Debug.Log($"Set Memory Limit to {ToRoundedMB(_memoryLimit, 2)}MB.");
+                return;
+            }
+
+            if (forceMobileMemoryLimit || heapMaxSize < mobileVsDesktopThreshold)
+            {
+                _memoryLimit = mobileMemoryLimit;
+                Debug.Log($"Set Memory Limit to {ToRoundedMB(_memoryLimit, 2)}MB.");
+                return;
+            }
+
+            _memoryLimit = desktopMemoryLimit;
+            Debug.Log($"Set Memory Limit to {ToRoundedMB(_memoryLimit, 2)}MB.");
+            return;
+        }
+
+        public static void InitMemoryLimit(uint size)
+        {
             _memoryLimit = size;
         }
 
-        public static uint GetMemoryLimit() 
+        public static uint GetMemoryLimit()
         {
             return _memoryLimit;
         }
 
-        public static double ToRoundedMB (ulong value, int places) {
-            if (places < 0) {
-                Debug.LogWarning("Places was less than 0. Setting to 0.");
-                places = 0;
+        //   <   UNITY_HEAP <?>  < MGD <MGD_FREE> <MGD_USED> >  <?>   >
+        public static bool HasEnoughMemoryToLoadBytes(long sizeToLoad)
+        {
+            if (_memoryLimit == 0)
+            {
+                Debug.LogWarning("Detected memory limit of 0. Allowing load of any size.");
+                return true;
             }
-            return Math.Round(value / 1024d / 1024d, places);
-        }
 
-        public static double ToRoundedMB (long value, int places) {
-            if (places < 0) {
-                Debug.LogWarning("Places was less than 0. Setting to 0.");
-                places = 0;
+            //This is the used and free memory in the managed heap. 
+            long totalManagedHeapSize = Profiler.GetMonoHeapSizeLong();
+
+            //Even if the object can fit in the managed heap, it still might grow by some amount.
+            //Note(Brandon): I am not sure what this amount truly is. This is just a guess based on the data I've seen so far.
+            long bumpedHeapSize = sizeToLoad + _padding + totalManagedHeapSize;
+
+            if (sizeToLoad < GetFreeMemoryInManagedHeap())
+            {
+                if (bumpedHeapSize >= (long)_memoryLimit)
+                {
+                    Debug.LogWarning(
+                    $"Bumped heap cannot fit. It would grow to {ToRoundedMB(bumpedHeapSize, 2)}MB, but is limited to {ToRoundedMB(_memoryLimit, 2)}MB.");
+                    return false;
+                }
+
+                Debug.Log($"Bumped heap ({ToRoundedMB(bumpedHeapSize, 2)}MB) can fit. Loading.");
+                return true;
             }
-            return Math.Round(value / 1024d / 1024d, places);
+
+            //Otherwise, the managed heap will double in size.
+            long doubledHeapSize = totalManagedHeapSize * 2 + _padding;
+
+            if (doubledHeapSize >= (long)_memoryLimit)
+            {
+                Debug.LogWarning(
+                    $"Doubled heap cannot fit. It would grow to {ToRoundedMB(totalManagedHeapSize * 2 + _padding, 2)}MB, but is limited to {ToRoundedMB(_memoryLimit, 2)}MB."
+                );
+                return false;
+            }
+
+            Debug.Log($"Doubled heap ({ToRoundedMB(totalManagedHeapSize * 2 + _padding, 2)}MB) can fit. Loading.");
+
+            return true;
         }
-
-        public static void LogMoreStats (string message) {
-            Debug.Log(GetMoreStats(message));
-        }
-
-        public static string GetMoreStats (string message) {
-            return ($"{message}: Managed heap used + free: {ToRoundedMB(Profiler.GetMonoHeapSizeLong(), 2)}MB\n"
-                + $"Managed heap used: {ToRoundedMB(Profiler.GetMonoUsedSizeLong(), 2)}MB\n"
-                + $"Highwater used: {ToRoundedMB(GetTotalUsedMemorySize(), 2)}MB.\n");
-        }
-
-        //   <UNITY_HEAP<?><MGD<MGD_FREE><MGD_USED>><?>>
-
-        public static long GetFreeMemoryInManagedHeap () {
+        
+        public static long GetFreeMemoryInManagedHeap()
+        {
             uint memoryLimit = GetMemoryLimit();
             long managedHeap = Profiler.GetMonoHeapSizeLong();
             long freeInManagedHeap = managedHeap - Profiler.GetMonoUsedSizeLong();
 
-            if (doLogStats) 
+            if (doLogStats)
             {
                 Debug.Log(
                     $"Free in Managed Heap: {ToRoundedMB(freeInManagedHeap, 2)}MB.\n"
@@ -121,13 +168,14 @@ namespace Komodo.AssetImport
 
             return freeInManagedHeap;
         }
-        
-        public static long GetMemoryLimitMinusManagedHeap () {
+
+        public static long GetMemoryLimitMinusManagedHeap()
+        {
             uint memoryLimit = GetMemoryLimit();
             long managedHeap = Profiler.GetMonoHeapSizeLong();
-            long memoryLimitMinusManagedHeap = ((long) memoryLimit) - managedHeap;
+            long memoryLimitMinusManagedHeap = ((long)memoryLimit) - managedHeap;
 
-            if (doLogStats) 
+            if (doLogStats)
             {
                 Debug.Log(
                     $"MemLimit minus MgdHeap: {ToRoundedMB(memoryLimitMinusManagedHeap, 2)}MB.\n"
@@ -138,63 +186,44 @@ namespace Komodo.AssetImport
             return memoryLimitMinusManagedHeap;
         }
 
-        public static bool HasEnoughMemoryToLoadBytes (long sizeToLoad) {
-            if (_memoryLimit == 0) {
-                Debug.LogWarning("Detected memory limit of 0. Allowing load of any size.");
-                return true;
-            }
-            
-            //This is the used and free memory in the managed heap. 
-            long totalManagedHeapSize = Profiler.GetMonoHeapSizeLong();
-
-            //Even if the object can fit in the managed heap, it still might grow by some amount.
-            //Note(Brandon): I am not sure what this amount truly is. This is just a guess based on the data I've seen so far.  
-            bool bumpedHeapCanFit = (sizeToLoad + _padding + totalManagedHeapSize) < (long) _memoryLimit;
-
-            if (sizeToLoad < GetFreeMemoryInManagedHeap() && !bumpedHeapCanFit) 
-            {
-                Debug.LogWarning(
-                    $"Bumped heap cannot fit. It would grow to {ToRoundedMB(sizeToLoad + _padding + totalManagedHeapSize, 2)}MB, but is limited to {ToRoundedMB(_memoryLimit, 2)}MB."
-                );
-                return false;
-            }
-
-            //Otherwise, the managed heap will double in size.
-            bool doubledHeapCanFit = (totalManagedHeapSize * 2 + _padding) < (long) _memoryLimit;
-
-            if (sizeToLoad >= GetFreeMemoryInManagedHeap() && !doubledHeapCanFit)
-            {
-                Debug.LogWarning(
-                    $"Doubled heap cannot fit. It would grow to {ToRoundedMB(sizeToLoad + _padding + totalManagedHeapSize, 2)}MB, but is limited to {ToRoundedMB(_memoryLimit, 2)}MB."
-                );
-                return false;
-            }
-
-            return true;
+        public static uint GetHeapMaxSize(bool doLogThis)
+        {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            return GetJSHeapMaxSize(doLogThis);
+# else 
+            Debug.LogWarning("Simulating GetHeapMaxSize(): returning maximum uint Value()");
+            return uint.MaxValue;
+#endif
         }
 
-        /*
-        * Naive implementation: check the Max JS Heap Size. If it exists, we are on Chrome, and if its value is less than an empirical amount, we should mark ourselves as mobile. 
-        */
-        public static void SetMemoryLimitForDevice () {
-            uint heapMaxSize = WebGLMemoryStats.GetHeapMaxSize(true);
+        public static uint GetTotalUsedMemorySize()
+        {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            return GetUsedMemorySize();
+# else 
+            Debug.LogWarning("Simulating GetTotalUsedMemorySize(): returning 0");
+            return 0;
+#endif
+        }
 
-            Debug.Log("iFrame JS Heap Size Limit: " + ToRoundedMB(heapMaxSize, 2) + "MB");
-
-            if (heapMaxSize == 0) {
-                _memoryLimit = 0;
+        public static double ToRoundedMB(ulong value, int places)
+        {
+            if (places < 0)
+            {
+                Debug.LogWarning("Places was less than 0. Setting to 0.");
+                places = 0;
             }
+            return Math.Round(value / 1024d / 1024d, places);
+        }
 
-            if (heapMaxSize < mobileVsDesktopThreshold) {
-                _memoryLimit = mobileMemoryLimit;
-                Debug.Log($"Set Memory Limit to {ToRoundedMB(_memoryLimit, 2)}MB.");
-                return;
+        public static double ToRoundedMB(long value, int places)
+        {
+            if (places < 0)
+            {
+                Debug.LogWarning("Places was less than 0. Setting to 0.");
+                places = 0;
             }
-
-            _memoryLimit = desktopMemoryLimit;
-            Debug.Log($"Set Memory Limit to {ToRoundedMB(_memoryLimit, 2)}MB.");
-            return;
+            return Math.Round(value / 1024d / 1024d, places);
         }
     }
-
 }
