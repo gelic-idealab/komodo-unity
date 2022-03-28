ca6e0457583fff104f7c19cd8fb66ce7a0f41d29
Barasakar <jlinpoughkeepsieday@gmail.com>
2022-03-28 12:34:07 -0500

    Finished documentation for most code in the FreeFlightController.cs and the KomodoMenu.cs

--- Komodo/Assets/Packages/KomodoCore/Runtime/Scripts/RuntimeSession/KomodoMenu.cs
+++ Komodo/Assets/Packages/KomodoCore/Runtime/Scripts/RuntimeSession/KomodoMenu.cs
@@ -9,27 +9,113 @@ namespace Komodo.Runtime
 {
     public class KomodoMenu : MonoBehaviour
     {
+        /** 
+         * @breif The erase is icon in the create tab, and it is a Button type. Once the KomodoMenu script is attached to a game object, 
+         * this variable need to be assigned through drag-and-drop.
+         * Hierarchy in the scene: KomodoMenu -> Panels -> CreateMenu -> ButtonsHorizontalLayout -> Erase.
+        */
+       [Tooltip("Hierarchy: KomodoMenu -> Panels -> CreateMenu -> ButtonsHorizontalLayout -> Erase")]
         public TabButton eraseTab;
 
+
+        /** 
+         * @breif The undo icon is in the create tab, and it is a Button type. Once the KomodoMenu script is attached to a game object, 
+         * this variable need to be assigned through drag-and-drop.
+         * Hierarchy: KomodoMenu -> Panels -> CreateMenu -> ButtonsHorizontalLayout -> Erase
+        */
+        [Tooltip("Hierarchy: KomodoMenu -> Panels -> CreateMenu -> ButtonsHorizontalLayout -> Undo")]
         public Button undoButton;
 
+
+        /** 
+         * @breif The draw icon is in the create tab, and it is a Button type. Once the KomodoMenu script is attached to a game object, 
+         * this variable need to be assigned through drag-and-drop.
+         * Hierarchy: Hierarchy: KomodoMenu -> Panels -> CreateMenu -> VerticalLayoutGroup -> Tabs -> Draw Tab
+        */
+        [Tooltip("Hierarchy: KomodoMenu -> Panels -> CreateMenu -> VerticalLayoutGroup -> Tabs -> Draw Tab")]
         public TabButton drawTab;
 
+
+        /** 
+         * @breif The brush icon is in the draw panel, and it is a Button type. Once the KomodoMenu script is attached to a game object, 
+         * this variable need to be assigned through drag-and-drop.
+         * Hierarchy: KomodoMenu -> Panels -> CreateMenu -> VerticalLayoutGroup -> DrawVerticalLayout -> ShapesGridLayout-> Brush
+        */
+        [Tooltip("Hierarchy: KomodoMenu -> Panels -> CreateMenu -> VerticalLayoutGroup -> DrawVerticalLayout -> ShapesGridLayout-> Brush")]
         public Toggle brushToggle;
 
+
+        /** 
+         * @breif The leaveAndRejoinButton is at the People tab, and it is a Button type. Once the KomodoMenu script is attached to a game object, 
+         * this variable need to be assigned through drag-and-drop.
+         * Hierarchy: KomodoMenu -> Panels -> PeopleMenu -> leaveAndRejoinButton
+        */
+        [Tooltip("Hierarchy: KomodoMenu -> Panels -> PeopleMenu -> leaveAndRejoinButton")]
         public Button leaveAndRejoinButton;
 
+
+        /**
+         * @breif The closeConnectionAndRejoinButton is at the People tab, and it is a Button type. 
+         * Once the KomodoMenu script is attached to a game object,this variable need to be assigned through drag-and-drop. 
+         * Hierarchy: KomodoMenu -> Panels -> PeopleMenu -> closeConnectionAndRejoinButton
+        */
+        [Tooltip("Hierarchy: KomodoMenu -> Panels -> PeopleMenu -> closeConnectionAndRejoinButton")]
         public Button closeConnectionAndRejoinButton;
         
+
+        /** 
+         * @breif The settingsTab represents the settings tab from our menu, and it is a TabButton type. 
+         * Once the KomodoMenu script is attached to a game object,this variable need to be assigned through drag-and-drop. 
+         * Hierarchy: KomodoMenu -> Tabs -> Settings
+        */
+        [Tooltip("Hierarchy: KomodoMenu -> Tabs -> Settings")]
         public TabButton settingsTab;
 
+
+        /** 
+         * @breif The peopleTab represents the people tab from our menu, and it is a TabButton type. 
+         * Once the KomodoMenu script is attached to a game object,this variable need to be assigned through drag-and-drop. 
+         * Hierarchy: KomodoMenu -> Tabs -> People
+        */
+        [Tooltip("Hierarchy: KomodoMenu -> Tabs -> People")]
         public TabButton peopleTab;
 
+
+        /** 
+         * @breif The interactTab represents the interact tab from our menu, and it is a TabButton type. 
+         * Once the KomodoMenu script is attached to a game object,this variable need to be assigned through drag-and-drop. 
+         * Hierarchy: KomodoMenu -> Tabs -> Interact
+        */
+        [Tooltip("Hierarchy: KomodoMenu -> Tabs -> Interact")]
         public TabButton interactTab;
 
+
+        /** 
+         * @breif The createTab represents the create tab from our menu, and it is a TabButton type. The create tab, when running in
+         * spectator/PC mode, will be disabled. 
+         * Once the KomodoMenu script is attached to a game object,this variable need to be assigned through drag-and-drop. 
+         * Hierarchy: KomodoMenu -> Tabs -> Create
+        */
+        [Tooltip("Hierarchy: KomodoMenu -> Tabs -> Create")]
         public TabButton createTab;
 
+
+        /** 
+         * @breif The instructorOnlyMenu represents the instructor menu from our desktop mode menu, and it is a GameObject type. 
+         * Once the KomodoMenu script is attached to a game object,this variable need to be assigned through drag-and-drop. 
+         * Hierarchy: KomodoMenu -> Panels -> InstructorOnlyMenu
+        */
+       [Tooltip("Hierarchy: KomodoMenu -> Panels -> InstructorOnlyMenu")]
         public GameObject instructorOnlyMenu;
+
+
+        /** 
+         * @breif The instructorMenuButton represents the instructor menu button in the settings tab from our desktop mode menu, 
+         * and it is a Button type. 
+         * Once the KomodoMenu script is attached to a game object,this variable need to be assigned through drag-and-drop. 
+         * Hierarchy: KomodoMenu -> Panels -> SettingsMenu -> NotCalibrating -> InstructorMenuButton
+        */
+        [Tooltip("Hierarchy: KomodoMenu -> Panels -> SettingsMenu -> NotCalibrating -> InstructorMenuButton")]
 
         public Button instructorMenuButton;
 
