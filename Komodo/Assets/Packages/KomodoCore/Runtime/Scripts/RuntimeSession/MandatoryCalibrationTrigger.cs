5bc4216cf1da34d59328ffbae83d06994e160c03
Barasakar <jlinpoughkeepsieday@gmail.com>
2022-06-21 12:26:42 -0500

    Finished commenting MandatoryCalibrationTrigger.cs

--- Komodo/Assets/Packages/KomodoCore/Runtime/Scripts/RuntimeSession/MandatoryCalibrationTrigger.cs
+++ Komodo/Assets/Packages/KomodoCore/Runtime/Scripts/RuntimeSession/MandatoryCalibrationTrigger.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Komodo.Runtime
-{
+{   
+    /// <summary>
+    /// This class contains a function that triggers Mandatory Height Calibration. However, I think this class is uncessary at this point. It requires further inspection though.
+    /// </summary>
     public class MandatoryCalibrationTrigger : MonoBehaviour
-    {
+    {   
+        /// <summary>
+        /// Trigger Mandatory Height Calibration event. 
+        /// </summary>
         void Start() 
         {
                 KomodoEventManager.TriggerEvent("MandatoryHeightCalibration");
