de8f326cc147a6b6c05efa8467273f95db0e2195
Barasakar <jlinpoughkeepsieday@gmail.com>
2022-06-23 12:51:11 -0500

    Commented GrabManager.cs

--- Komodo/Assets/GrabManager.cs
+++ Komodo/Assets/GrabManager.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class does not seem like it is being used.
+/// </summary>
 public class GrabManager : MonoBehaviour
 {
     // Start is called before the first frame update
