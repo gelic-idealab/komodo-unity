7b5695a7c2b4d15efb7fa197ae3d1a332a496070
Barasakar <jlinpoughkeepsieday@gmail.com>
2022-06-27 11:13:29 -0500

    Finished commenting ToggleExpandability.cs

--- Komodo/Assets/Packages/KomodoCore/Runtime/Scripts/RuntimeSession/ToggleExpandability.cs
+++ Komodo/Assets/Packages/KomodoCore/Runtime/Scripts/RuntimeSession/ToggleExpandability.cs
@@ -4,12 +4,30 @@ using UnityEngine;
 
 namespace Komodo.Runtime
 {
+    /// <summary>
+    /// This class controls the expandability of Komodo UI menu.
+    /// </summary>
     public class ToggleExpandability : MonoBehaviour
     {
+        /// <summary>
+        /// An array of game objects that are expanders.
+        /// </summary>
         public GameObject[] expanders;
+        
+        /// <summary>
+        /// An array of game objects that are collapsers.
+        /// </summary>
         public GameObject[] collapsers;
+
+        /// <summary>
+        /// And array of game objects that are panels.
+        /// </summary>
         public GameObject[] panels;
 
+        /// <summary>
+        /// Convert an array of collapsers to expandable.
+        /// </summary>
+        /// <param name="doExpand">a boolean value for deciding whether to expand or not. True for expanding, false for collapsing.</param>
         public void ConvertToExpandable(bool doExpand)
         {
             foreach (GameObject collapser in collapsers)
@@ -26,6 +44,9 @@ namespace Komodo.Runtime
             }
         }
 
+        /// <summary>
+        /// Set an array of collapsers to always expanded.
+        /// </summary>
         public void ConvertToAlwaysExpanded()
         {
             foreach (GameObject collapser in collapsers)
