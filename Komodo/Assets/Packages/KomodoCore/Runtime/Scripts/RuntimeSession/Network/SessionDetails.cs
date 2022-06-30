d009af7d081a7195c1eb20f4db756d5b6c86f1a7
Barasakar <jlinpoughkeepsieday@gmail.com>
2022-06-30 16:25:01 -0500

    Finished commenting scripts about session details and states.

--- Komodo/Assets/Packages/KomodoCore/Runtime/Scripts/RuntimeSession/Network/SessionDetails.cs
+++ Komodo/Assets/Packages/KomodoCore/Runtime/Scripts/RuntimeSession/Network/SessionDetails.cs
@@ -3,18 +3,60 @@ using Komodo.AssetImport;
 
 namespace Komodo.Runtime
 {
+    /// <summary>
+    /// The struct that contains detailed information about a session.
+    /// </summary>
     [System.Serializable]
     public struct SessionDetails
     {
+        /// <summary>
+        /// A list that contains data of model that will be imported. 
+        /// </summary>
         public List<ModelDataTemplate.ModelImportData> assets;
+        
+        /// <summary>
+        /// The build version.
+        /// </summary>
         public string build;
+
+        /// <summary>
+        /// The course ID.
+        /// </summary>
         public int course_id;
+
+        /// <summary>
+        /// Location of the session.
+        /// </summary>
         public string create_at;
+
+        /// <summary>
+        /// Description of the session.
+        /// </summary>
         public string description;
+
+        /// <summary>
+        /// The end time of the session.
+        /// </summary>
         public string end_time;
+
+        /// <summary>
+        /// The ID of the session.
+        /// </summary>
         public int session_id;
+
+        /// <summary>
+        /// The name of the session.
+        /// </summary>
         public string session_name;
+
+        /// <summary>
+        /// The start time of the session.
+        /// </summary>
         public string start_time;
+
+        /// <summary>
+        /// A list of users in the session.
+        /// </summary>
         public List<User> users;
     }
 }
--- Komodo/Assets/Packages/KomodoCore/Runtime/Scripts/RuntimeSession/SessionState.cs
+++ Komodo/Assets/Packages/KomodoCore/Runtime/Scripts/RuntimeSession/SessionState.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 namespace Komodo.Runtime
 {
+    /// <summary>
+    /// The state of the current session; information includes numbers of clients, which scene it is, and whether the session is being recorded.
+    /// </summary>
     [System.Serializable]
     public class SessionState
     {
+        /// <summary>
+        /// A list that stores clients' IDs.
+        /// </summary>
         public int[] clients;
+
         public EntityState[] entities;
+
+        /// <summary>
+        /// The current scene of the session.
+        /// </summary>
         public int scene;
+
+        /// <summary>
+        /// Whether the session is being recorded.
+        /// </summary>
         public bool isRecording;
     }
 }
